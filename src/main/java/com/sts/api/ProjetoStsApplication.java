4738d4f605e429880c46a2b31c25814211546165
Jefferson Ayub <jefferson.ayub@gmail.com>
2019-06-24 11:22:04 -0300

    Arquivos iniciais do proejto

---
+++ src/main/java/com/sts/api/ProjetoStsApplication.java
@@ -0,0 +1,13 @@
+package com.sts.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoStsApplication.class, args);
+	}
+
+}
